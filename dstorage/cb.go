b32f8f165c3d43d0f41b9609f9993848f047d541
Md Miran Ahmed Ansari <mdmiranahmedansari@Mds-MacBook-Air.local>
2021-12-19 14:04:53 +0530

    status bar

--- dstorage/cb.go
+++ dstorage/cb.go
@@ -3,35 +3,24 @@ package dStorage
 import (
 	"fmt"
 	"github.com/0chain/gosdk/core/transaction"
-	"gopkg.in/cheggaaa/pb.v1"
 	"os"
 	"sync"
 )
 
 type StatusCB struct {
-	b                *pb.ProgressBar
 	wg               *sync.WaitGroup
 	success          bool
-	allocUnderRepair bool
 }
 
 func (s *StatusCB) Started(allocationId, filePath string, op int, totalBytes int) {
-	s.b = pb.StartNew(totalBytes)
-	s.b.Set(0)
 }
 
 func (s *StatusCB) InProgress(allocationId, filePath string, op int, completedBytes int, data []byte) {
-	s.b.Set(completedBytes)
 }
 
 func (s *StatusCB) Error(allocationID string, filePath string, op int, err error) {
-	if s.b != nil {
-		s.b.Finish()
-	}
 	s.success = false
-	if !s.allocUnderRepair {
-		defer s.wg.Done()
-	}
+	defer s.wg.Done()
 
 	var errDetail interface{} = "Unknown Error"
 	if err != nil {
@@ -42,13 +31,8 @@ func (s *StatusCB) Error(allocationID string, filePath string, op int, err error
 }
 
 func (s *StatusCB) Completed(allocationId, filePath string, filename string, mimetype string, size int, op int) {
-	if s.b != nil {
-		s.b.Finish()
-	}
 	s.success = true
-	if !s.allocUnderRepair {
-		defer s.wg.Done()
-	}
+	defer s.wg.Done()
 	fmt.Println("Status completed callback. Type = " + mimetype + ". Name = " + filename)
 }
 
@@ -65,7 +49,6 @@ func (s *StatusCB) CommitMetaCompleted(request, response string, txn *transactio
 
 func (s *StatusCB) RepairCompleted(filesRepaired int) {
 	defer s.wg.Done()
-	s.allocUnderRepair = false
 	fmt.Println("Repair file completed, Total files repaired: ", filesRepaired)
 }
 
