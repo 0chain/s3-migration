1f28ddada86022bd870f257a9c02f00e5950d994
pewssh <pewssh@gmail.com>
2024-09-29 20:19:17 +0545

    check for access token

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -129,9 +129,9 @@ var migrateCmd = &cobra.Command{
 			}
 		}
 
-		if accessToken == "" {
+		if accessToken == "" && (source=="google_drive" || source =="dropbox") {
 			if accessToken = util.GetAccessToken(); accessToken == "" {
-				return errors.New("Missing Access Token")
+				return errors.New("missing access token")
 			}
 		}
 		if bucket == "" && source == "s3" {
