ad6191f9caae77f6d195491b04136f2328403e17
pewssh <pewssh@gmail.com>
2024-12-24 18:02:21 +0545

    updated for older and newer than

--- migration/migrate.go
+++ migration/migrate.go
@@ -144,6 +144,8 @@ func InitMigration(mConfig *MigrationConfig) error {
 		dataSourceStore, err = dropbox.GetDropboxClient(
 			util.GetAccessKeyFromEnv(),
 			mConfig.WorkDir,
+			NewerThan:       newerThanPtr,
+			OlderThan:       olderThanPtr,
 		)
 	} else if mConfig.Source == "google_drive" {
 		// use client id instead of access token to prevent expiry time
