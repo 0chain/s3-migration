108615e39981d5d212879c119447dab2bee0a611
pewssh <pewssh@gmail.com>
2025-03-05 03:35:05 +0545

    endpoint updated

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -263,7 +263,7 @@ var migrateCmd = &cobra.Command{
 		}
 
 		if source == "azure" {
-			connectionString = fmt.Sprintf("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%sEndpointSuffix=core.windows.net", accountName, accessKey)
+			connectionString = fmt.Sprintf("DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s;EndpointSuffix=core.windows.net", accountName, accessKey)
 			if err := util.SetAzureCredentials(connectionString, accountName, containerName); err != nil {
 				return err
 			}
